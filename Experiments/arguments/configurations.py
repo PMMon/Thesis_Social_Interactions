a6edc947062f26105a7f7b2d3193b716ed22ab7f
PhilippMond <40031584+PhilippMond@users.noreply.github.com>
2020-06-17 10:06:23 +0200

    arguments

--- Experiments/arguments/configurations.py
+++ Experiments/arguments/configurations.py
@@ -1,17 +1,12 @@
+# ============================= Description =============================
+# Predefine parameters for the Social LSTM Model and the Vanilla LSTM Model
+# =======================================================================
+
 def load_args(args):
+    """
+    Loads pre-defines parameters either for the Social LSTM Model or the Vanilla LSTM Model
+    """
     if args.args_set == "social-lstm":
-        args.encoder_h_dim = 64     #32
-        args.decoder_h_dim = 32     #1
-        args.emb_dim = 32           #32
-        args.batch_size = 128       #128
-        args.lr = 0.001
-        args.wd = 0.0
-        args.dropout = 0.3         #0.3
-        args.model_type = "social-lstm"
-        args.lstm_pool = True
-        args.plot_name = "social-lstm"
-        return args
-    elif args.args_set == "lstm":
         args.encoder_h_dim = 64
         args.decoder_h_dim = 32
         args.emb_dim = 32
@@ -19,6 +14,19 @@ def load_args(args):
         args.lr = 0.001
         args.wd = 0.0
         args.dropout = 0.3
+        args.model_type = "social-lstm"
+        args.lstm_pool = True
+        args.plot_name = "social-lstm"
+        return args
+
+    elif args.args_set == "lstm":
+        args.encoder_h_dim = 64
+        args.decoder_h_dim = 32
+        args.emb_dim = 32
+        args.batch_size = 128
+        args.lr = 0.001
+        args.wd = 0.0
+        args.dropout = 0.0
         args.model_type = "lstm"
         args.lstm_pool = False
         args.plot_name = "lstm"
