6df0efad3faf4748a11b987b38f7d2ddbbc3863a
PhilippMond <40031584+PhilippMond@users.noreply.github.com>
2020-06-17 09:51:41 +0200

    Implementation of Models

--- Experiments/model/BaseModel.py
+++ Experiments/model/BaseModel.py
@@ -2,32 +2,49 @@ import torch
 import torch.nn as nn
 
 class BaseModel(nn.Module):
-    def __init__(self,
-                 device = 'cpu',
-                 float_type=torch.float64):
+    """
+    Base class for different Trajectory Prediction Models. Implements basic functionality, such as switching to
+    different modes or saving the Model.
+    """
+    def __init__(self,device = 'cpu',float_type=torch.float64):
         super(BaseModel, self).__init__()
+
         self.__dict__.update(locals())
         self.losses = ["G_Loss"]
-
 
         self.type(float_type)
         self.gen()
         self.get_name()
 
+
     def test(self):
+        """
+        Change mode to testing/validation
+        """
         self.eval()
         self.mode = "test"
         return self
+
+
     def get_name(self):
+        """
+        Get name of Model
+        """
         self.model = self.__class__.__name__
 
 
     def gen(self):
+        """
+        Change  mode to training
+        """
         self.mode = "gen"
         self.train()
         return self
 
     def save(self, opt, loss, epochs, path):
+        """
+        Save Model, the configurations of the chosen optimizer, the history of losses and the last epoch
+        """
         print("Saving model to path %s ..." %path)
         torch.save({"model_state_dict": self.state_dict(), "optimizer_state_dict": opt.state_dict(), "loss": loss, "epochs": epochs}, path)
 
--- Experiments/model/modules.py
+++ Experiments/model/modules.py
@@ -1,6 +1,3 @@
-"""
-This script implements the bicycle GAN
-"""
 import sys, os
 sys.path.append(os.path.abspath("."))
 import torch
@@ -9,9 +6,18 @@ import argparse
 
 from helper.bool_flag import bool_flag
 from model.BaseModel import BaseModel
-from model.model_modules import make_mlp, get_noise, Decoder, Encoder
+from model.model_modules import make_mlp, Decoder, Encoder
+
+# ===================== Description =======================
+# Implementation of different trajectory prediction models
+# =========================================================
 
 class LINEAR(BaseModel):
+    """
+    Linear model: uses a fully connected layer to predict the next relative position/velocity of each pedestrian.
+    The predicted position is then linearly projected towards future time steps. Hence, the model predicts exclusively linear
+    future trajectories.
+    """
     def __init__(self, args,
                  float_type = torch.float64,
                  device="gpu",
@@ -26,10 +32,11 @@ class LINEAR(BaseModel):
         self.mlp_dim = mlp_dim
         self.input_dim = input_dim
 
+        # Specify losses for model: Average Displacement Error (ADE), Final Displacement Error (FDE) and the average of these two losses (AV)
         self.losses.extend(["G_L2", "G_ADE", "G_FDE", "G_AV"])
 
         self.fc = nn.Sequential(
-                nn.Linear((self.obs_len-1) * self.input_dim, self.mlp_dim),    # initially (self.obs_len-1) * self.input_dim. -> used for relative coordinations (dxdy), otherwise use (self.obs_len)*self.input_dim
+                nn.Linear((self.obs_len-1) * self.input_dim, self.mlp_dim),
                 nn.Tanh(),
                 nn.Dropout(self.dropout),
                 nn.Linear(self.mlp_dim, self.mlp_dim ),
@@ -38,15 +45,39 @@ class LINEAR(BaseModel):
                 nn.Linear(self.mlp_dim, self.input_dim))
 
     def forward(self, inputs):
+        """
+        Implements forward run of model. Note that the model predicts the relative position of the time step obs_len + 1.
+        It then maps this relative distance on the remaining time steps obs_len + 2, ..., obs_len + pred_len
+        :param inputs: Dictionary that holds information about the observed trajectories
+        :return: Dictionary with information about the predicted trajectories in absolute coordinates
+        """
         input_abs = inputs["in_xy"]
         input_rel = inputs["in_dxdy"]
         batch_size = input_abs.size(1)
+
+        # Predict relative position of next time step (t = obs_len + 1)
         v = self.fc(input_rel.permute(1, 0, 2).reshape(batch_size, -1)).unsqueeze(0)
+
+        # Map predicted distance to remaining time steps
         time = torch.arange(1, self.pred_len+1).to(v).view(-1, 1, 1).repeat(1, batch_size, 1)
         out_xy = time * v + input_abs[-1].unsqueeze(0)
+
         return {"out_xy": out_xy}
 
+
 class LSTM(BaseModel):
+    """
+    This class implements the Vanilla LSTM model (if flag --lstm_pool is set to 'False' AND --model_type is set to 'lstm') and
+    the Social LSTM model (if flag --lstm_pool is set to 'True' OR --model_type is set to 'social-lstm').
+
+    A conceptional overview of the Vanilla LSTM model can be found in chapter 3.3.2 of the bachelor's thesis:
+    P. Mondorf: 'Modeling Social Interactions for Pedestrian Trajectory Prediction on Real and Synthetic Datasets'.
+    Technical University of Munich, 2020.
+
+    A conceptional overview of the Social LSTM model can be found in this paper:
+    A. Alahi, K. Goel, V. Ramanathan, A. Robicquet, L. Fei-Fei, and S. Savarese. 'Social LSTM: Human Trajectory Prediction in Crowded Space'.
+    In: CVPR (2016), pp. 961–971. Link: http://cvgl.stanford.edu/papers/CVPR16_Social_LSTM.pdf
+    """
     def __init__(self, args,
                  float_type=torch.float64,
                  device = "gpu",
@@ -72,9 +103,8 @@ class LSTM(BaseModel):
         self.grid_size = args.grid_size
         self.final_position = args.final_position
 
-
-        self.losses.extend(["G_L2",  "G_ADE", "G_FDE", "G_AV"])
-
+        # Specify losses for model: Average Displacement Error (ADE), Final Displacement Error (FDE) and the average of these two losses (AV)
+        self.losses.extend(["G_L2",  "G_ADE", "G_FDE", "G_AV", "G_ADE_nl_regions"])
 
         self.encoder = Encoder(
             encoder_h_dim=self.encoder_h_dim,
@@ -100,13 +130,12 @@ class LSTM(BaseModel):
                                )
 
         if self.mlp_decoder_needed():
+            # Use MLP to match the dimensions between encoder and decoder
             self.encoder2decoder = make_mlp(
                 [self.encoder_h_dim, self.mlp_dim, self.decoder_h_dim],
                     activation='relu',
                     dropout = self.dropout)
 
-    def init_c(self, batch_size):
-        return torch.zeros((1, batch_size, self.decoder_h_dim))
 
     def mlp_decoder_needed(self):
         if (self.encoder_h_dim != self.decoder_h_dim):
@@ -114,9 +143,23 @@ class LSTM(BaseModel):
         else:
             return False
 
+
+    def init_c(self, batch_size):
+        """
+        Initialize empty cell state for the decoder
+        :param batch_size: Size of batch
+        :return: Cell state
+        """
+        return torch.zeros((1, batch_size, self.decoder_h_dim))
+
+
     def forward(self, inputs):
-
-
+        """
+        Implements forward run of model. Note that the model is based on a lstm-based encoder-decoder archictecture, where the
+        input is processed by an encoder and the decoder predicts the future trajectory of each pedestrian.
+        :param inputs: Dictionary that holds information about the observed trajectories
+        :return: Dictionary with information about the predicted trajectories
+        """
         batch_size = inputs["in_xy"].size(1)
 
         _,h = self.encoder(inputs["in_dxdy"])
@@ -126,28 +169,30 @@ class LSTM(BaseModel):
 
         c = self.init_c(batch_size).to(inputs["in_xy"])
 
-
-        #last position
+        # Last observed position
         x0=inputs["in_xy"][-1]
         v0=inputs["in_dxdy"][-1]
         state_tuple = (h, c)
 
-        #provide information about final position
+        # Provide information about final position
         if self.final_position:
             dest = inputs["gt"][-1]
         else:
-            dest = 0    #torch.zeros(x0.shape)
+            dest = 0
 
+        # If data is loaded with information about all pedestrians in a scene, pass this information to the decoder
         if "seq_start_end" in inputs.keys():
             seq_start_end = inputs["seq_start_end"]
         else:
             seq_start_end = []
 
+        # If missing data is padded, pass information about which trajectories are padded to the decoder
         if "pred_check" in inputs.keys():
             pred_check = inputs["pred_check"]
         else:
             pred_check = torch.ones(batch_size,1)
 
+        # Predict future trajectories
         out = self.decoder(last_pos=x0,
                 last_pos_rel=v0,
                 state_tuple=state_tuple,
@@ -164,36 +209,42 @@ class LSTM(BaseModel):
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser("Trajectory Prediction Basics")
-    # Config about Model
+
+    # Configs for Model
     parser.add_argument("--model_name", default="", type=str, help="Define model name for saving")
-    parser.add_argument("--model_type", default="lstm", type=str, help="Define model type. Either Linear or LSTM Model")
+    parser.add_argument("--model_type", default="lstm", type=str,help="Define type of model. Choose either: linear, lstm or social-lstm")
     parser.add_argument("--save_model", default=True, type=bool_flag, help="Save trained model")
-    parser.add_argument("--load_model", default=False, type=bool_flag, help="specify whether to load existing model")
-    parser.add_argument("--pool_every_timestep", default=False, type=bool_flag,help="specify whether to enable social pooling")
-    parser.add_argument("--pooling", default="social_pooling", type=str, help="specify pooling method")
+    parser.add_argument("--load_model", default=False, type=bool_flag, help="Specify whether to load existing model")
+    parser.add_argument("--lstm_pool", default=False, type=bool_flag, help="Specify whether to enable social pooling")
+    parser.add_argument("--pooling_type", default="social_pooling", type=str, help="Specify pooling method")
+    parser.add_argument("--neighborhood_size", default=10.0, type=float, help="Specify neighborhood size to one side")
+    parser.add_argument("--grid_size", default=10, type=int, help="Specify grid size")
+    parser.add_argument("--args_set", default="", type=str, help="Specify predefined set of configurations for respective model. Choose either: lstm or social-lstm")
 
-    # Config about data-preparation
-    parser.add_argument("--dataset_name", default="hotel", type=str, help="Specify dataset")
-    parser.add_argument("--obs_len", default=8, type=int, help="Specify observed length")
-    parser.add_argument("--pred_len", default=12, type=int, help="specify predicted length")
-    parser.add_argument("--data_augmentation", default=True, type=bool_flag,
-                        help="Specify whether or not you want to use data augmentation")
+    # Configs for data-preparation
+    parser.add_argument("--dataset_name", default="to_be_defined", type=str, help="Specify dataset")
+    parser.add_argument("--dataset_type", default="square", type=str, help="Specify dataset-type. For real datasets choose: 'real'. For synthetic datasets choose either 'square' or 'rectangle'")
+    parser.add_argument("--obs_len", default=8, type=int, help="Specify length of observed trajectory")
+    parser.add_argument("--pred_len", default=12, type=int, help="Specify length of predicted trajectory")
+    parser.add_argument("--data_augmentation", default=True, type=bool_flag, help="Specify whether or not you want to use data augmentation")
     parser.add_argument("--batch_norm", default=False, type=bool_flag, help="Batch Normalization")
-    parser.add_argument("--max_num", default=10000, type=int, help="specify maximum number of ids")
-    parser.add_argument("--skip", default=20, type=int, help="specify skipping rate")
-    parser.add_argument("--PhysAtt", default="", type=str, help="specify physicalAtt")
+    parser.add_argument("--max_num", default=1000000, type=int, help="Specify maximum number of ids")
+    parser.add_argument("--skip", default=20, type=int, help="Specify skipping rate")
+    parser.add_argument("--PhysAtt", default="", type=str, help="Specify physicalAtt")
+    parser.add_argument("--padding", default=True, type=bool_flag, help="Specify if padding should be active")
+    parser.add_argument("--final_position", default=False, type=bool_flag, help="Specify whether final positions of pedestrians should be passed to model or not")
 
-    # Config about training, val, testing
-    parser.add_argument("--batch_size", default=32, type=int, help="specify batch size")
-    parser.add_argument("--wd", default=0.03, type=float, help="specify weight decay")
-    parser.add_argument("--lr", default=0.001, type=float, help="specify learning rate")
-    parser.add_argument("--decoder_h_dim", default=16, type=int, help="specify decoder_h_dim")
-    parser.add_argument("--encoder_h_dim", default=16, type=int, help="specify encoder_h_dim")
-    parser.add_argument("--emb_dim", default=8, type=int, help="specify embedding dimension of encoder")
-    parser.add_argument("--num_epochs", default=30, type=int, help="specify number of epochs")
-    parser.add_argument("--dropout", default=0.0, type=float, help="specify dropout rate")
-    parser.add_argument("--num_layers", default=1, type=int, help="specify number of layers for LSTM")
-    parser.add_argument("--optim", default="Adam", type=str, help="specify optimizer")
+    # Configs for training, validation, testing
+    parser.add_argument("--batch_size", default=32, type=int, help="Specify batch size")
+    parser.add_argument("--wd", default=0.03, type=float, help="Specify weight decay")
+    parser.add_argument("--lr", default=0.001, type=float, help="Specify learning rate")
+    parser.add_argument("--encoder_h_dim", default=64, type=int, help="Specify hidden state dimension h of encoder")
+    parser.add_argument("--decoder_h_dim", default=32, type=int, help="Specify hidden state dimension h of decoder")
+    parser.add_argument("--emb_dim", default=32, type=int, help="Specify dimension of embedding")
+    parser.add_argument("--num_epochs", default=250, type=int, help="Specify number of epochs")
+    parser.add_argument("--dropout", default=0.0, type=float, help="Specify dropout rate")
+    parser.add_argument("--num_layers", default=1, type=int, help="Specify number of layers of LSTM/Social LSTM Model")
+    parser.add_argument("--optim", default="Adam", type=str, help="Specify optimizer. Choose either: adam, rmsprop or sgd")
 
     # Get arguments
     args = parser.parse_args()
