f638032b1a760eeaf9e989daba7353bd29dc6f8a
PhilippMond <40031584+PhilippMond@users.noreply.github.com>
2020-06-17 09:47:11 +0200

    rm unnecessary files

--- Experiments/download_dataset.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-wget https://vision.in.tum.de/webshare/u/dendorfp/TrajectoryPredictionData/datasets.zip
-unzip datasets.zip -d datasets
-rm datasets.zip
--- Experiments/download_dataset_mac.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-curl https://vision.in.tum.de/webshare/u/dendorfp/TrajectoryPredictionData/datasets.zip -OL datasets.zip
-mkdir datasets
-tar -xzvf datasets.zip -C datasets
-rm datasets.zip
