5ad4d036f7e35717b7e121682433c773d5897632
PhilippMond <40031584+PhilippMond@users.noreply.github.com>
2020-06-17 09:48:34 +0200

    Run Experiments on Synthetic Data

--- Experiments/run_slurm_social_force.sh
+++ Experiments/run_slurm_social_force.sh
@@ -6,16 +6,16 @@
 #SBATCH --mail-type=all
 #SBATCH --output=slurm/slurm-%j.out
 
-# 0 1 2 4 6
-# 0.2171 0.4343 0.8686 1.303 1.7371 2.171 2.6058
-# 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0 1.1 1.2 1.3 1.4 1.5 1.6 1.7 1.8 1.9 2.0
-# for S-LSTM: "python main/main.py --model_type social-lstm --lstm_pool true --args_set social-lstm --num_epochs 200 --data_augmentation true --phase train --socialforce true --neighborhood_size 10 --plot_name social-lstm --V0 $i --sigma $j";
-# for LSTM: "python main/main.py --model_type lstm --lstm_pool false --args_set lstm --num_epochs 200 --data_augmentation true --phase train --socialforce true --plot_name lstm --V0 $i --sigma $j";
+# Example bash script to run experiment with models on synthetic datasets.
+# Values of V0 used in thesis: 0 1 2 4 6
+# Values of sigma used in thesis: 0.2171 0.4343 0.8686 1.303 1.7371 2.171 2.6058
+# for S-LSTM: "python main/Experiment_Control.py --model_type social-lstm --lstm_pool true --args_set social-lstm --num_epochs 200 --data_augmentation true --phase train --socialforce true --neighborhood_size 10 --V0 $i --sigma $j --padding True";
+# for LSTM: "python main/Experiment_Control.py --model_type lstm --lstm_pool false --args_set lstm --num_epochs 200 --data_augmentation true --phase train --socialforce true --V0 $i --sigma $j --padding True";
 
-for i in 6
+for i in 0 1 2 4 6
 do
-  for j in 2.6058
+  for j in 0.2171 0.4343 0.8686 1.303 1.7371 2.171 2.6058
   do
-    eval "python main/Experiment_Control.py --model_type social-lstm --lstm_pool True --args_set social-lstm --num_epochs 250 --data_augmentation true --phase test --socialforce true --neighborhood_size 10 --grid_size 10 --plot_name social-lstm --padding True --final_position True --show_traj true --histo false --nl_coarse False --visualize_classified False --V0 $i --sigma $j";
+    eval "python main/Experiment_Control.py --model_type social-lstm --lstm_pool True --args_set social-lstm --phase train --num_epochs 250 --socialforce true --V0 $i --sigma $j --data_augmentation true --neighborhood_size 10 --grid_size 10 --padding True --final_position False --show_traj true --nl_ADE False --analyse_coll_avoidance False --CDF_CollAvoid False --nl_classified False --visualize_classified False";
   done
 done
