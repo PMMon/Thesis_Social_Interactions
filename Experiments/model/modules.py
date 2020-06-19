28c7f944eee53c98ca05d806eed60a4a3148c5a3
PhilippMond <40031584+PhilippMond@users.noreply.github.com>
2020-06-19 09:23:18 +0200

    Implementation of Models

--- Experiments/model/modules.py
+++ Experiments/model/modules.py
@@ -1,5 +1,6 @@
 import sys, os
 sys.path.append(os.path.abspath("."))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
 import torch
 import torch.nn as nn
 import argparse
